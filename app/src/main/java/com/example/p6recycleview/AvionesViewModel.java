5353726fdfe877ae0fd1490661ac0b5afa5b9bb1
jose <jbermejop01@elpuig.xeill.net>
2020-11-12 17:13:50 +0100

    Initial Commit.

--- app/src/main/java/com/example/p6recycleview/AvionesViewModel.java
+++ app/src/main/java/com/example/p6recycleview/AvionesViewModel.java
@@ -1,13 +1,31 @@
 package com.example.p6recycleview;
 
 import android.app.Application;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.p6recycleview.databinding.ViewholderAvionBinding;
+
+import java.util.List;
 
 public class AvionesViewModel extends AndroidViewModel {
 
+    AvionRepositorio avionRepositorio;
+
+    MutableLiveData<List<Avion>> listAvionesMutableLiveData = new MutableLiveData<>();
+
     public AvionesViewModel(@NonNull Application application) {
         super(application);
+
+        avionRepositorio = new AvionRepositorio();
+        listAvionesMutableLiveData.setValue(avionRepositorio.obtener());
+    }
+
+    MutableLiveData<List<Avion>> obtener(){
+        return listAvionesMutableLiveData;
     }
 }
